12250ec48aa7c61ac5278e1ffb5636c7b7bb4704
perhapz <perhapz@gmail.com>
2012-02-25 16:08:29 +0800

    add attach block support

--- showhide.user.js
+++ showhide.user.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name           ShowHide
 // @namespace      null
-// @description    Move hidden block to top
-// @version        1.0.0
+// @description    Move hidden & attachment block to top
+// @version        1.0.1
 // @updateURL      https://raw.github.com/perhapz/FFconf/master/showhide.user.js
 // @include        http://64.78.167.63/bbs/viewthread.php*
 // @include        http://64.78.167.62/bbs/viewthread.php*
 // ==/UserScript==
-var hide = document.getElementsByClassName('showhide');
+var hide = document.getElementsByClassName('showhide')[0];
+var attach = document.getElementsByClassName('postattachlist')[0];
 if (hide) {
   var title = document.getElementById('threadtitle');
-  title.appendChild(hide[0]);
+  title.appendChild(hide);
+  title.appendChild(hide);
 }
